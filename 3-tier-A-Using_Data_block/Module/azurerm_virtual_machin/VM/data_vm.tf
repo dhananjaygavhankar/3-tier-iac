f1120e8cbe4e0443315b9285e733e5219e28cfa4
Dhananjay Gavhankar <dhananjaygavhankar@outlook.com>
2025-08-21 20:23:33 +0530

    changes

--- 3-tier-A-Using_Data_block/Module/azurerm_virtual_machin/VM/data_vm.tf
+++ 3-tier-A-Using_Data_block/Module/azurerm_virtual_machin/VM/data_vm.tf
@@ -9,13 +9,13 @@ data "azurerm_key_vault" "key_vault" {
 }
  
 data "azurerm_key_vault_secret" "username" {
-  # name         = var.username
-  name         = "T0d000l0in"
+  name         = var.username
+  # name         = "T0d000l0in"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 data "azurerm_key_vault_secret" "password" {
-  # name         = var.password
-  name         = "T0d000pasw0rd"
+  name         = var.password
+  # name         = "T0d000pasw0rd"
   key_vault_id = data.azurerm_key_vault.key_vault.id
 }
 
